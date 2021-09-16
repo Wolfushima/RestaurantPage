b0e7f07d027c104f03cf46861f0df1f8290fdaa5
Adolfo Herrera <valherdolf@gmail.com>
2021-09-16 16:51:41 -0400

    Added a click event every time you click on each nav link

--- src/navbar.js
+++ src/navbar.js
@@ -81,6 +81,9 @@ function navSlide() {
             else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
+            link.addEventListener("click", () => {
+                menuIcon.click();
+            })
         })
     })
 }
